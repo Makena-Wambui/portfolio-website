c8966fac4e2e0aa1530937857b0e688a2bad8e07
Makena-Wambui <iammakena@gmail.com>
2025-04-24 18:13:14 +0300

    Worked on Style Switcher and Dark/Light Mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,43 @@ navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
   navToggle.classList.toggle("animate-toggle");
 });
+
+/* Style Switcher */
+const styleSwitcher = document.getElementById("style-switcher");
+
+const switcherToggle = document.getElementById("switcher-toggle");
+
+const switcherClose = document.getElementById("switcher-close");
+
+switcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.add("show-switcher");
+});
+
+switcherClose.addEventListener("click", () => {
+  styleSwitcher.classList.remove("show-switcher");
+});
+
+/* Theme Colors */
+const colors = document.querySelectorAll(".style-switcher-color");
+colors.forEach((color) => {
+  color.onclick = () => {
+    const activeColor = color.style.getPropertyValue("--hue");
+
+    colors.forEach((c) => c.classList.remove("active-color"));
+    color.classList.add("active-color");
+
+    document.documentElement.style.setProperty("--hue", activeColor);
+  };
+});
+
+/* Theme Mode */
+
+let currentTheme = "light";
+document.body.className = currentTheme;
+
+document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
+  input.addEventListener("change", () => {
+    currentTheme = input.value;
+    document.body.className = currentTheme;
+  });
+});
